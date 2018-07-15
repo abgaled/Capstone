6bb04ed70a7f6ffd37f132ca01210488362bc8c1
Jill <jillmarie_inc0622@yahoo.com>
2018-07-15 23:37:58 +0800

    22nd attempt
    What's New
    -nakikita na yung problems na nasend XD(kaso wala pa yung acknowledgement button)
    -proposal form(kaso di ko alam kung bkit get pa din sya, naka-post nman at
     may insert)(madami pa babaguhin)

--- app/modules/office/problems/routes.js
+++ app/modules/office/problems/routes.js
@@ -6,11 +6,51 @@ var db = require('../../../lib/database')();
 
 router.get('/',(req, res) => {
     console.log('=================================');
-    console.log('OFFICE: PROBLEMS');
+    console.log('OFFICE: PROBLEM STATEMENT-PREVIOUS');
     console.log('=================================');
 
-    res.render('office/problems/views/problems');
+    var queryString = `SELECT * FROM tbl_problemstatement pr
+    JOIN tbl_problemcategory prcat ON pr.int_problemCategID=prcat.int_problemCategID
+    ORDER BY pr.int_problemID DESC`
 
+
+    db.query(queryString,(err, results, fields) => {
+        if (err) console.log(err);
+        
+
+    res.render('office/problems/views/problems',{tbl_problemstatement:results});
+    });
+
+});
+
+router.post('/viewprobcategory',(req,res) => {
+    console.log('=================================');
+    console.log('BARANGAY: PROBLEM STATEMENT-PREVIOUS-SPECIFIC CATEGORY');
+    console.log('=================================');
+
+        if(`${req.body.problem_category}`== "All"){
+            console.log("ALLLLLLLL"); 
+            var queryString = `SELECT * FROM tbl_problemstatement pr
+            JOIN tbl_problemcategory prcat ON pr.int_problemCategID=prcat.int_problemCategID
+            ORDER BY pr.int_problemID DESC`
+        
+            db.query(queryString,(err, results, fields) => {
+
+                res.redirect('office/problems/views/problems',{tbl_problemstatement:results});
+                });
+        }
+        else{
+            console.log("ELSEEEE")
+            var queryString = `SELECT * FROM tbl_problemstatement pr
+            JOIN tbl_problemcategory prcat ON pr.int_problemCategID=prcat.int_problemCategID
+            WHERE pr.int_problemCategID=${req.body.problem_category}
+            ORDER BY pr.int_problemID DESC`
+
+            db.query(queryString,(err, results, fields) => {
+               
+                res.redirect('office/problems/views/problems',{tbl_problemstatement:results});
+                });
+        }
 });
 
 module.exports = router;
--- app/modules/office/proposals/routes.js
+++ app/modules/office/proposals/routes.js
@@ -8,9 +8,99 @@ router.get('/',(req, res) => {
     console.log('=================================');
     console.log('OFFICE: PROPOSALS');
     console.log('=================================');
+    
+    var queryString =`SELECT * FROM tbl_projectcategory WHERE enum_projCategState = 'Active'`
+    
+    db.query(queryString, (err, results, fields) => {
+        console.log(results);
+        if (err) console.log(err);
+        // console.log(results);
+        res.render('office/proposals/views/proposals', {tbl_projectcategory: results});
 
-    res.render('office/proposals/views/proposals');
+});
+});
 
+router.post('/',(req, res) => {
+    console.log('=================================');
+    console.log('OFFICE: PROPOSALS');
+    console.log('=================================');
+    
+    var queryString = `INSERT INTO \`tbl_projectproposal\` (
+        
+        \`varchar_projectName\`,
+        \`int_projectCategID\`,
+        \`varchar_releaseLocation\`,
+        \`varchar_projectRationale\`,
+        \`text_projectDescription\`,
+        \`text_expectedOutput\`,
+        \`int_allotedSlot\`,
+        \`decimal_estimatedBudget\`,
+        \`decimal_individualBudget\`,
+        \`enum_proposalStatus\`)
+                
+        VALUES(
+        "${req.body.projectname}",
+        "${req.body.projectcategory}",
+        "${req.body.releaselocation}",
+        "${req.body.projectrationale}",
+        "${req.body.projectdescription}",
+        "${req.body.expectedoutput}",
+        "${req.body.allotedslot}",
+        "${req.body.estimatedbudget}",
+        "${req.body.individualbudget}",
+        "Accepted");`;
+
+        db.query(queryString, (err, results, fields) => {        
+            if (err) throw err;    
+            console.log(results);
+       
+        var queryString1 =`SELECT * FROM tbl_projectproposal ORDER BY int_projectID DESC LIMIT 0,1`
+
+        db.query(queryString1, (err, results1, fields) => {        
+            if (err) throw err;
+
+            var toproject = results1[0];
+
+            var queryString2 = `INSERT INTO \`tbl_project\` (
+                \`int_projectID\`,
+                \`date_startDate\`,
+                \`date_endDate\`,
+                \`date_releaseDate\`,
+                \`enum_projectState\`)
+                
+                VALUES(
+                "${project.int_projectID}",
+                "${req.body.startdate}",
+                "${req.body.enddate}",
+                "${req.body.releasedate}",
+                "Active");`;
+
+                        db.query(queryString2, (err, results2, fields) => {        
+                            if (err) throw err;
+
+                            console.log(results2);
+                    
+                res.redirect('/admin/maintenance/projects');
+        });
+        });
+
+    });
+});
+
+router.get('/submittedproposals',(req, res) => {
+    console.log('=================================');
+    console.log('OFFICE: PROPOSALS');
+    console.log('=================================');
+    
+    var queryString =`SELECT * FROM tbl_projectcategory WHERE enum_projCategState = 'Active'`
+    
+    db.query(queryString, (err, results, fields) => {
+        console.log(results);
+        if (err) console.log(err);
+        // console.log(results);
+        res.render('office/proposals/views/submittedproposals', {tbl_projectcategory: results});
+
+});
 });
 
 module.exports = router;
